38663484693b48d0e11ad46f13c67fbe95af9b33
vitorcastro <v170rc@hotmail.com>
2026-02-03 14:33:27 +0100

    asset model

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,20 @@
 class Asset < ApplicationRecord
   belongs_to :user
+
+  CATEGORIES = [
+    'Bank Account',
+    'Stocks',
+    'Crypto',
+    'Property',
+    'Vehicle',
+    'Collectible',
+    'Other'
+  ].freeze
+
+
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :name, presence: true
+  validates :value, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+
 end
