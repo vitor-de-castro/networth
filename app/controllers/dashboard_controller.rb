9df57ae5397a8a975a8cdf65fe7adb38fca2f631
vitorcastro <v170rc@hotmail.com>
2026-02-03 15:36:47 +0100

    index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   #before_action :authenticate_user!
+
   def index
+    @assets = current_user.assets.order(created_at: :desc)
+    @total_net_worth = current_user.assets.sum(:value)
   end
 end
