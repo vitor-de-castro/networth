0265b336caedd30592d77d54bbed23a73f23fedd
vitorcastro <v170rc@hotmail.com>
2026-02-18 21:20:41 +0100

    currency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :assets, dependent: :destroy
 
+  CURRENCIES = ['USD', 'EUR', 'GBP', 'JPY', 'CHF', 'CAD', 'AUD'].freeze
+
+  validates :currency, inclusion: { in: CURRENCIES }, allow_nil: true
 end
